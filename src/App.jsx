1aee203ba45f1e028bf073ad3655dad1c21948b1
RehanulBary <rehanulbary198@gmail.com>
2026-03-19 16:25:34 +0600

    message

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { getWelcomeMessage } from "./messages.js";
 export default function App(index) {
   return (
     <main style={{ fontFamily: "sans-serif", maxWidth: 640, margin: "40px auto" }}>
-      <h1>Simple React App</h1>
+      <h1>Simple React App {index}</h1>
       <p>{getWelcomeMessage("Repo-Orcha")}</p>
     </main>
   );
